d0f8c0382592408ee85ba2b917b2b2d8ea862154
Deacon Kearl <deaconkearl@gmail.com>
2026-06-02 20:20:27 -0600

    Added orthoFit

--- src/com/magicdeaks/heatcapacity/HeatCapacityFitter.java
+++ src/com/magicdeaks/heatcapacity/HeatCapacityFitter.java
@@ -17,11 +17,11 @@ public abstract class HeatCapacityFitter {
      * @return A map linking each physical model to its fitted coefficients.
      */
     public static Map<LowTHeatCapacityModel, FitResult> lowTPolyFit(double[] temperatures, double[] heatCapacities) {
-        int nPoints = temperatures.length;
+        int numPoints = temperatures.length;
 
         Map<LowTHeatCapacityModel, FitResult> results = new EnumMap<>(LowTHeatCapacityModel.class);
 
-        if (nPoints != heatCapacities.length) {
+        if (numPoints != heatCapacities.length) {
             throw new IllegalArgumentException("temperatures and heatCapacities arrays must have same length");
         }
         for (double heatCapacity : heatCapacities) {
@@ -34,9 +34,9 @@ public abstract class HeatCapacityFitter {
             double[] powers = model.getPowers();
             int numParams = powers.length;
 
-            double[][] designMatrix = new double[nPoints][numParams];
+            double[][] designMatrix = new double[numPoints][numParams];
 
-            for (int point = 0; point < nPoints; point++) {
+            for (int point = 0; point < numPoints; point++) {
                 for (int powerIdx = 0; powerIdx < numParams; powerIdx++) {
                     designMatrix[point][powerIdx] = Math.pow(temperatures[point], powers[powerIdx]) / heatCapacities[point];
                 }
@@ -48,7 +48,7 @@ public abstract class HeatCapacityFitter {
             double[] y = new double[numParams];
             for (int i = 0; i < numParams; i++) {
                 double sum = 0;
-                for (int j = 0; j < nPoints; j++) {
+                for (int j = 0; j < numPoints; j++) {
                     sum += transposed[i][j];
                 }
                 y[i] = sum;
@@ -57,7 +57,7 @@ public abstract class HeatCapacityFitter {
             double[] coefficients = MatrixMath.solveLinearSystem(ata, y);
 
             double sumOfSquaredRelativeErrors = 0.0;
-            for (int point = 0; point < nPoints; point++) {
+            for (int point = 0; point < numPoints; point++) {
                 double calculatedC = 0.0;
                 for (int powerIdx = 0; powerIdx < numParams; powerIdx++) {
                     calculatedC += coefficients[powerIdx] * Math.pow(temperatures[point], powers[powerIdx]);
@@ -67,13 +67,148 @@ public abstract class HeatCapacityFitter {
                 sumOfSquaredRelativeErrors += relativeError * relativeError;
             }
 
-            double pctRMS = Math.sqrt(sumOfSquaredRelativeErrors / nPoints) * 100.0;
+            double pctRMS = Math.sqrt(sumOfSquaredRelativeErrors / numPoints) * 100.0;
 
             results.put(model, new FitResult(coefficients, pctRMS));
 
         }
 
         return results;
+    }
+
+    public static double[] fitOrthogonalPolynomial(double[] temperatures, double[] heatCapacities, int startPower, int powerIncrement, int totalTerms) {
+        int numberOfPoints = temperatures.length;
+
+        if (numberOfPoints != heatCapacities.length) {
+            throw new IllegalArgumentException("temperatures and heatCapacities arrays must have the same length.");
+        }
+
+        double minTemperature = Double.MAX_VALUE;
+        double maxTemperature = -Double.MAX_VALUE;
+
+        for (int i = 0; i < numberOfPoints; i++) {
+            if (heatCapacities[i] <= 0.0) {
+                throw new IllegalArgumentException("heatCapacities must be positive.");
+            }
+            if (temperatures[i] < minTemperature) minTemperature = temperatures[i];
+            if (temperatures[i] > maxTemperature) maxTemperature = temperatures[i];
+        }
+
+        double[] currentOrthoPoly = new double[numberOfPoints];
+        double[] prevOrthoPoly = new double[numberOfPoints];
+        double[] prevPrevOrthoPoly = new double[numberOfPoints];
+        double[] weightsSquared = new double[numberOfPoints];
+
+        for (int i = 0; i < numberOfPoints; i++) {
+            currentOrthoPoly[i] = Math.pow(temperatures[i], startPower);
+            double weight = 1.0 / heatCapacities[i];
+            weightsSquared[i] = weight * weight;
+        }
+
+        double tempMidpoint = minTemperature + (maxTemperature - minTemperature) / 2.0;
+        double tempScaleFactor = maxTemperature - tempMidpoint;
+
+        double[] scaledTemperatures = new double[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++) {
+            scaledTemperatures[i] = (temperatures[i] - tempMidpoint) / tempScaleFactor;
+        }
+
+        int maxIterations = (int) Math.ceil((double) (totalTerms - startPower + powerIncrement) / powerIncrement) + 2;
+        double[] prevPrevBasisCoeffs = new double[maxIterations];
+        double[] prevBasisCoeffs = new double[maxIterations];
+        double[] currentBasisCoeffs = new double[maxIterations];
+        double[] orthoFitCoeffs = new double[maxIterations];
+
+        double recurrenceAlpha = 0, recurrenceBeta = 0, prevNormSquared = 1.0;
+        double targetIterations = (double) (totalTerms - startPower + powerIncrement) / powerIncrement;
+
+        int currentIteration = 1;
+        currentBasisCoeffs[0] = 1;
+
+        // Orthogonal fitting iteration loop
+        while (true) {
+            double sumPolySq = 0;
+            double sumXPolySq = 0;
+            double sumXPolyPrevPoly = 0;
+            double sumYPoly = 0;
+
+            for (int i = 0; i < numberOfPoints; i++) {
+                if (currentIteration != 1) {
+                    currentOrthoPoly[i] = (Math.pow(temperatures[i], powerIncrement) - recurrenceAlpha) * prevOrthoPoly[i] - recurrenceBeta * prevPrevOrthoPoly[i];
+                }
+
+                double polySq = currentOrthoPoly[i] * currentOrthoPoly[i];
+                double xIncr = Math.pow(scaledTemperatures[i], powerIncrement);
+
+                sumPolySq += weightsSquared[i] * polySq;
+                sumXPolySq += weightsSquared[i] * xIncr * polySq;
+                sumXPolyPrevPoly += weightsSquared[i] * xIncr * currentOrthoPoly[i] * prevOrthoPoly[i];
+                sumYPoly += weightsSquared[i] * currentOrthoPoly[i] * heatCapacities[i];
+
+                prevPrevOrthoPoly[i] = prevOrthoPoly[i];
+                prevOrthoPoly[i] = currentOrthoPoly[i];
+            }
+
+            recurrenceAlpha = sumXPolySq / sumPolySq;
+            double currentFitStep = sumYPoly / sumPolySq;
+            recurrenceBeta = sumXPolyPrevPoly / prevNormSquared;
+            prevNormSquared = sumPolySq;
+
+            for (int i = 0; i < currentIteration; i++) {
+                orthoFitCoeffs[i] += currentFitStep * currentBasisCoeffs[i];
+            }
+
+            if (currentIteration >= targetIterations) break;
+
+            int lastIterCount = currentIteration;
+            currentIteration += 1;
+            currentBasisCoeffs[currentIteration] = 1;
+
+            for (int i = 0; i < lastIterCount; i++) {
+                if (currentIteration - i != 0) {
+                    prevPrevBasisCoeffs[currentIteration - i - 1] = prevBasisCoeffs[currentIteration - i - 1];
+                }
+                prevBasisCoeffs[currentIteration - i] = currentBasisCoeffs[currentIteration - i];
+            }
+
+            for (int i = 0; i < lastIterCount; i++) {
+                currentBasisCoeffs[i] = 0;
+                if (i != 0) {
+                    currentBasisCoeffs[i] += prevBasisCoeffs[i - 1];
+                }
+
+                currentBasisCoeffs[i] -= recurrenceAlpha * prevBasisCoeffs[i];
+                if (currentIteration >= 3 && currentIteration - 2 >= i) {
+                    currentBasisCoeffs[i] -= recurrenceBeta * prevPrevBasisCoeffs[i];
+                }
+            }
+        }
+
+        // Matrix generation for back-transformation to standard polynomial coefficients
+        double[][] binomialMatrix = new double[totalTerms + 1][totalTerms + 1];
+        double[] finalStandardCoeffs = new double[totalTerms + 1];
+
+        for (int i = 0; i < totalTerms + 1; i++) {
+            binomialMatrix[0][i] = 1;
+            binomialMatrix[i][0] = 1;
+        }
+
+        for (int i = 1; i < totalTerms + 1; i++) {
+            for (int j = 1; j < totalTerms + 1; j++) {
+                binomialMatrix[i][j] = binomialMatrix[i - 1][j] + binomialMatrix[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < totalTerms + 1; i++) {
+            int binomialOffset = 0;
+            for (int j = i; j < totalTerms + 1; j++) {
+                double sign = ((j + i) % 2 == 0) ? 1.0 : -1.0;
+                finalStandardCoeffs[i] += (binomialMatrix[i][binomialOffset] * orthoFitCoeffs[j] * Math.pow(tempMidpoint, j - i) * sign) / Math.pow(tempScaleFactor, j);
+                binomialOffset++;
+            }
+        }
+
+        return finalStandardCoeffs;
     }
 
     public static void print(Map<? extends HeatCapacityModel, FitResult> fittedModels) {
@@ -91,6 +226,5 @@ public abstract class HeatCapacityFitter {
             System.out.println();
         });
     }
-
 
 }
--- src/com/magicdeaks/heatcapacity/Main.java
+++ src/com/magicdeaks/heatcapacity/Main.java
@@ -3,18 +3,33 @@ package com.magicdeaks.heatcapacity;
 import com.magicdeaks.heatcapacity.models.LowTHeatCapacityModel;
 import com.magicdeaks.heatcapacity.util.DataReader;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     static void main() {
         double[][] data = DataReader.readCSV("data.csv");
+        Scanner scanner = new Scanner(System.in);
+        double[] lowTUpperBound = { 0 };
 
-        double[] temperatures = data[0];
-        double[] heatCapacities = data[1];
+        do {
+            try {
+                System.out.print("Low T Upper Bound: ");
+                lowTUpperBound[0] = Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number...");
+            }
+        } while (lowTUpperBound[0] == 0);
+
+        double[] lowTemperatures = Arrays.stream(data[0])
+                .filter(val -> val < lowTUpperBound[0])
+                .toArray();
+        double[] lowHeatCapacities = Arrays.copyOf(data[1], lowTemperatures.length);
 
         long startTime = System.nanoTime();
 
-        Map<LowTHeatCapacityModel, FitResult> fittedLowT = HeatCapacityFitter.lowTPolyFit(temperatures, heatCapacities);
+        Map<LowTHeatCapacityModel, FitResult> fittedLowT = HeatCapacityFitter.lowTPolyFit(lowTemperatures, lowHeatCapacities);
 
         long endTime = System.nanoTime();
 
